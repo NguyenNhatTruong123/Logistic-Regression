484c80b4556e77df515a27888ea01c9cbc732625
NguyenNhatTruong123 <49356137+NguyenNhatTruong123@users.noreply.github.com>
2020-09-09 08:50:07 +0700

    Update Logistic_regression.py

--- Logistic_regression.py
+++ Logistic_regression.py
@@ -6,9 +6,11 @@ from sklearn.linear_model import LogisticRegression
 data_test=pd.read_csv("test.csv")
 data_train=pd.read_csv("train.csv")
 
+#Remove inconsequential columns
 data_train = data_train.drop(['PassengerId','Name','Ticket','Cabin','Parch'], axis=1)
 data_test = data_test.drop(['PassengerId','Name','Ticket','Cabin','Parch'], axis=1)
 
+#Digitizing non-number coulumns and drop them
 sex_train=pd.get_dummies(data_train["Sex"])
 sex_test=pd.get_dummies(data_test["Sex"])
 
@@ -21,8 +23,6 @@ data_test=pd.concat([data_test,sex_test],axis=1)
 data_train["Age"].fillna(data_train["Age"].min(),inplace=True)
 data_test["Age"].fillna(data_test["Age"].min(),inplace=True)
 
-data_test["Fare"].fillna(data_test["Fare"].min(),inplace=True)
-
 embarked_train=pd.get_dummies(data_train["Embarked"])
 embarked_test=pd.get_dummies(data_test["Embarked"])
 
@@ -32,6 +32,11 @@ data_test=data_test.drop(["Embarked"],axis=1)
 data_train=pd.concat([data_train,embarked_train],axis=1)
 data_test=pd.concat([data_test,embarked_test],axis=1)
 
+
+#Fill in the blank on Fare column, can use min, max or mean of column 
+data_test["Fare"].fillna(data_test["Fare"].min(),inplace=True)
+
+#Training data using Logistic Regression in sklearn library
 y_train=data_train["Survived"].values
 x_train=data_train.drop(["Survived"],axis=1).values
 
